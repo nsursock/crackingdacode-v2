8fcc769941ceebf4a85a0781473fee5b2627f0e4
Nicolas Sursock <nicolas.sursock@gmail.com>
2023-11-18 16:55:59 +0200

    tried to improve cls on lighthouse

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    uglify: {
+      options: {
+        compress: {
+          unused: true
+        }
+      },
+      my_target: {
+        files: {
+          'dist/assets/_main.js': ['dist/assets/_main.js'],
+        }
+      }
+    },
+    critical: {
+      dist: {
+        options: {
+          base: './',
+          dimensions: [{
+            width: 1300,
+            height: 900
+          },
+          {
+            width: 500,
+            height: 900
+          }]
+        },
+        files: [
+          { src: ['dist/index.html'], dest: 'dist/index.html' }
+        ]
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-critical');
+
+
+  // Default tasks.
+  grunt.registerTask('default', ['critical']);
+};
