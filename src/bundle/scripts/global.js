a3436e541ed88e68bc616b0d1e6fdf11499f6dd9
Nicolas Sursock <nicolas.sursock@gmail.com>
2023-11-21 12:52:15 +0200

    fixed bug with scrolling article

--- src/bundle/scripts/global.js
+++ src/bundle/scripts/global.js
@@ -5,8 +5,8 @@ export default () => ({
   lastScrollTop: window.scrollY || document.documentElement.scrollTop,
   isScrollingUp: false,
   prevPercent: 0,
-  thispercent: 0,
-  currStep: '',
+  percent: 0,
+  currStep: [],
 
   registerEvent(label, type) {
     // if (process.env.NODE_ENV.startsWith('prod')) {
@@ -44,52 +44,53 @@ export default () => ({
   async init() {
     window.addEventListener('scroll', () => {
       this.prevPercent = this.percent;
-
+    
       const st = window.scrollY || document.documentElement.scrollTop;
       this.isScrollingUp = st <= this.lastScrollTop;
       this.lastScrollTop = st <= 0 ? 0 : st;
-
+    
       const target = document.querySelector('#article');
-
+    
       if (target) {
         const winTop = window.scrollY || document.documentElement.scrollTop;
         const targetBottom = target.offsetTop + target.scrollHeight;
         const windowBottom = winTop + window.outerHeight;
-
+    
         this.percent = Math.min(
           Math.round(
             100 -
-            ((targetBottom - windowBottom + window.outerHeight / 3) /
-              (targetBottom - window.outerHeight + window.outerHeight / 3)) *
-            100
+              ((targetBottom - windowBottom + window.outerHeight / 3) /
+                (targetBottom - window.outerHeight + window.outerHeight / 3)) *
+              100
           ),
           100
         );
-
+    
         const updateStep = (step, label) => {
           if (
             this.percent >= step.start &&
             this.percent < step.end &&
             !this.currStep.includes(label)
           ) {
-            this.currStep = `article-${step.end}`;
-            this.registerEvent(`${step.end}%`, 'scroll');
+            this.currStep.push(label); // Update to an array
+            this.registerEvent(`${step.start}%`, 'scroll');
           }
         };
-
+    
         const steps = [
+          // { start: 0, end: 25 },
           { start: 25, end: 50 },
           { start: 50, end: 75 },
           { start: 75, end: 100 },
         ];
-
+    
         steps.forEach((step) => updateStep(step, `${step.start}`));
-
-        if (this.percent === 100 && this.currStep.includes('75')) {
-          this.currStep = 'article-100';
+    
+        if (this.percent === 100 && !this.currStep.includes('100')) {
+          this.currStep.push('100');
           this.registerEvent('100%', 'scroll');
         }
       }
-    }, false)
+    }, false);
   }
 })
